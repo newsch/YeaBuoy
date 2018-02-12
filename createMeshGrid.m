60c753ba78ae77ab7a87455eaa04b612da71cedd
Nathan Estill <nestill@olin.edu>
2018-02-12 11:54:05 -0500

    updating MeshGrid

--- createMeshGrid.m
+++ createMeshGrid.m
@@ -1,7 +1,8 @@
 function mesh = createMeshGrid(dy,dz,boat)
+    mesh.xs = 0:1:1;
     mesh.ys = -boat.HB:dy:boat.HB;        % meters
     mesh.zs = 0:dz:boat.D;                % meters
-    [mesh.ygrid,mesh.zgrid] = meshgrid(mesh.ys,mesh.zs); %create an xy meshgrid
+    [mesh.xgrid,mesh.ygrid,mesh.zgrid] = meshgrid(mesh.xs,mesh.ys,mesh.zs); %create an xy meshgrid
     total_area = boat.W * boat.D;       % find the area of the subsection
     mesh.dA = total_area / numel(mesh.ygrid); % find the meshes
 end
